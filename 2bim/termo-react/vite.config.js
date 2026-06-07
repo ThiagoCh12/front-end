3f1756e3788b792ea11f20562cdc7d777a2bf088
ThiagoCh12 <thiagocampanholi11@gmail.com>
2026-06-07 13:09:40 -0300

    deploy termo-react via GitHub Actions

--- 2bim/termo-react/vite.config.js
+++ 2bim/termo-react/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// base precisa bater com o caminho onde o Pages serve o build:
+// https://thiagoch12.github.io/front-end/termo/
 export default defineConfig({
+  base: '/front-end/termo/',
   plugins: [react()],
 })
